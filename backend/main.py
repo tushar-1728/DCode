b193e9e78adcdf164c53bd7ef86f10f18ee85ffa
Divya Raj <divya.raj@kgpian.iitkgp.ac.in>
2024-04-05 23:28:29 +0530

    Added User profile link

--- backend/main.py
+++ backend/main.py
@@ -1,31 +1,26 @@
 from flask import Flask, request, redirect, render_template, flash, render_template_string
 from flask.helpers import url_for
+from config import API_USER_INFO, API_USER_STATUS
 
 import os
 import json, requests
 
 app = Flask("DCODE")
-app.secret_key = "divyaraj"
+app.secret_key = os.environ.get("FLASK_SECRET_KEY")
 
 def getHandleColor(rating):
     if rating<1200:
-        return "gray"
+        return "#36454F"
     elif 1200<=rating<1400:
         return "green"
     elif 1400<=rating<1600:
         return "cyan"
     elif 1600<=rating<1900:
-        return "blue"
+        return "#00008B"
     elif 1900<=rating<2100:
         return "violet"
-    elif 2100<=rating<2300:
+    elif 2100<=rating<2400:
         return "orange"
-    elif 2300<=rating<2400:
-        return "orange"
-    elif 2400<=rating<2600:
-        return "red"
-    elif 2600<=rating<3000:
-        return "red"
     else:
         return "red"
     
@@ -65,7 +60,7 @@ def codeforces():
         tags = json.load(f)
 
     if request.method == "POST" and userhandle:
-        url = f"https://codeforces.com/api/user.info?handles={userhandle}&checkHistoricHandles=false"
+        url = API_USER_INFO.format(userhandle)
         response = requests.get(url)
         # Parse JSON response
         data = response.json()
@@ -73,12 +68,18 @@ def codeforces():
         # Check if API response is successful
         if data.get("status") == "OK":
             # Accessing rank, maxRating, and rating
-            rank = data["result"][0]["rank"]
-            max_rating = data["result"][0]["maxRating"]
-            rating = data["result"][0]["rating"]
-            ratingColor = getHandleColor(rating)
+            if "rank" in data["result"][0]:
+                rank = data["result"][0]["rank"]
+                max_rating = data["result"][0]["maxRating"]
+                rating = data["result"][0]["rating"]
+                ratingColor = getHandleColor(rating)
+            else:
+                rank = "Unrated"
+                rating = 0
+                max_rating = 0
+                ratingColor = getHandleColor(rating)
 
-            url = f"https://codeforces.com/api/user.status?handle={userhandle}&from=1&count=9999"
+            url = API_USER_STATUS.format(userhandle)
             response = requests.get(url)
             probs = response.json()
 
@@ -96,7 +97,7 @@ def codeforces():
             # Render the template with userhandle included
             return render_template("codeforces.html", userhandle=userhandle, rank=rank, max_rating=max_rating, rating=rating, problems=problems, tags=tags['tags'], probs=probs["result"], ratingColor=ratingColor, user_verdicts=user_verdicts, correct_cnt=correct_cnt)
     # Render the template with userhandle unchanged
-    return render_template("codeforces.html", userhandle=userhandle, problems=problems, tags=tags['tags'])
+    return render_template("codeforces.html", userhandle=userhandle, problems=problems, tags=tags['tags'], correct_cnt=0, user_verdicts={})
 
 
 
