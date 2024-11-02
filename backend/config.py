23f65f905088ad1e54660eb1ddef0cfd6b3f404c
Tushar Raj <43772524+tushar-1728@users.noreply.github.com>
2024-11-02 19:32:52 +0000

    added config file

---
+++ backend/config.py
@@ -0,0 +1,13 @@
+# Codeforces
+API_CODEFORCES_USER_INFO = "https://codeforces.com/api/user.info?handles={}&checkHistoricHandles=false"
+API_CODEFORCES_USER_PROBLEM_STATUS = "https://codeforces.com/api/user.status?handle={}&from=1&count=9999"
+API_CODEFORCES_PROBLEMSET_LIST = "https://codeforces.com/api/problemset.problems"
+API_CODEFORCES_USER_CONTEST_INFO = "https://codeforces.com/api/user.rating?handle={}"
+
+# AtCoder
+API_ATCODER_PROBLEM_INFO = "https://kenkoooo.com/atcoder/resources/problems.json"
+API_ATCODER_PROBLEM_DIFFICULTY = "https://kenkoooo.com/atcoder/resources/problem-models.json"
+API_ATCODER_USER_CONTEST_INFO = "https://atcoder.jp/users/{}/history/json"
+API_ATCODER_USER_PROBLEM_STATUS = "https://kenkoooo.com/atcoder/atcoder-api/v3/user/submissions?user={}&from_second={}"
+
+#CodeChef
